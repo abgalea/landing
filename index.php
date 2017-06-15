27aa78b461f76ac6934170096e833f704e64d9dc
Abrahm Galeano <aagaleano@hotmail.com>
2017-06-15 19:37:30 -0300

    Add files via upload
    add new files

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.12.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.12.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-terrenos3-128x96.png" type="image/x-icon">
+  <meta name="description" content="Grupo Carlés ">
+  <title>Grupo Carlés</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/mobirise/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise-slider/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section class="mbr-slider mbr-section mbr-section--no-padding carousel slide" data-ride="carousel" data-wrap="true" data-interval="false" id="slider-0" style="background-color: rgb(255, 255, 255);">
+    <div class="mbr-section__container">
+        <div>
+            
+            <div class="carousel-inner" role="listbox">
+                <div class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--bg-adapted item dark center mbr-section--full-height active" style="background-image: url(assets/images/gropup-2000x875.jpg);">
+                    <div class="mbr-box__magnet mbr-box__magnet--center-center mbr-box__magnet--sm-padding">
+                                            
+                        <div class=" container">
+                            
+                            <div class="row">
+                                <div class=" col-md-8 col-md-offset-2">  
+
+                                <div class="mbr-hero">
+                                    
+
+                                    
+                                </div>
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <a data-app-prevent-settings="" class="left carousel-control" role="button" data-slide="prev" href="#slider-0">
+                <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a data-app-prevent-settings="" class="right carousel-control" role="button" data-slide="next" href="#slider-0">
+                <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="engine"><a rel="external" href="https://mobirise.com">Mobirise Web Page Generator</a></section><section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="features1-1" style="background-color: rgb(232, 192, 126);">
+    
+    <div class="mbr-section__container mbr-section__container--std-top-padding mbr-section__container--sm-bot-padding mbr-section-title container" style="padding-top: 93px;">
+        <div class="mbr-header mbr-header--center mbr-header--wysiwyg row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <h3 class="mbr-header__text">GRUPO CARLÉS</h3>
+                
+            </div>
+        </div>
+    </div>
+    <div class="mbr-section__container container">
+        <div class="mbr-section__row row">
+            <div class="mbr-section__col col-xs-12 col-sm-4">
+                <div class="mbr-section__container mbr-section__container--center mbr-section__container--last" style="padding-bottom: 93px;">
+                    <figure class="mbr-figure"><img src="assets/images/lct-600x296.jpg" class="mbr-figure__img"></figure>
+                </div>
+                
+                
+                
+            </div>
+            <div class="mbr-section__col col-xs-12 col-sm-4">
+                <div class="mbr-section__container mbr-section__container--center mbr-section__container--last" style="padding-bottom: 93px;">
+                    <figure class="mbr-figure"><img src="assets/images/lci-600x296.jpg" class="mbr-figure__img"></figure>
+                </div>
+                
+                
+                
+            </div>
+            
+            <div class="mbr-section__col col-xs-12 col-sm-4">
+                <div class="mbr-section__container mbr-section__container--center mbr-section__container--last" style="padding-bottom: 93px;">
+                    <figure class="mbr-figure"><img src="assets/images/lcc-600x296.jpg" class="mbr-figure__img"></figure>
+                </div>
+                
+                
+                
+            </div>
+            
+            
+            
+            
+            
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="assets/bootstrap-carousel-swipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/mobirise/js/script.js"></script>
+  
+  
+</body>
+</html>
